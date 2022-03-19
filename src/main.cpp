ab333f71bef230612b7d181e94e2380e0cd09866
Marcus Eriksson <marcus.jr.eriksson@gmail.com>
2022-03-19 18:47:23 +0100

    A lot of prototyping and PoC, update to C++17

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,13 @@
 #include "data_processing.h"
 #include "simulator.h"
 
+// #include "ADQAPI.h"
+
 #include "GL/gl3w.h"
 #include <GLFW/glfw3.h>
+
+#include <cstdlib>
+#include <filesystem>
 
 static char text[1024 * 16] =
 R"({
@@ -99,8 +104,8 @@ int main(int, char **)
 
     ImGui::StyleColorsDark();
 
-    bool show_demo_window = false;
-    bool show_plot_demo_window = false;
+    bool show_imgui_demo_window = true;
+    bool show_implot_demo_window = false;
 
     DataAcquisitionSimulator acquisition_a;
     DataAcquisitionSimulator acquisition_b;
@@ -122,11 +127,76 @@ int main(int, char **)
     sine_b.noise_std_dev = 0.01;
     sine_b.sampling_frequency = 2.5e9;
 
+    auto Initialize = [&]()
+    {
+        acquisition_a.Initialize(10000, 2, sine_a);
+        processing_a.Initialize();
+        acquisition_b.Initialize(10000, 30, sine_b);
+        processing_b.Initialize();
+    };
+
+    auto Start = [&]()
+    {
+        Initialize();
+        processing_a.Start();
+        processing_b.Start();
+    };
+
+    auto Stop = [&]()
+    {
+        processing_a.Stop();
+        processing_b.Stop();
+    };
+
+    // void *adq_cu = CreateADQControlUnit();
+    // if (adq_cu == NULL)
+    // {
+    //     printf("Failed to create an ADQControlUnit.\n");
+    // }
+
+    // struct ADQInfoListEntry *adq_list = NULL;
+    // int nof_devices = 0;
+    // if (!ADQControlUnit_ListDevices(adq_cu, &adq_list, (unsigned int *)&nof_devices))
+    // {
+    //     printf("Failed to list devices.\n");
+    // }
+    // printf("Found %d devices.\n", nof_devices);
+
+    // struct ADQInfoListEntry dummy_adq_list[4];
+    // for (int i = 0; i < sizeof(dummy_adq_list) / sizeof(dummy_adq_list[0]); ++i)
+    // {
+    //     dummy_adq_list[i].ProductID = PID_ADQ3;
+    // }
+
+    // if (nof_devices == 0)
+    // {
+    //     adq_list = dummy_adq_list;
+    //     nof_devices = sizeof(dummy_adq_list) / sizeof(dummy_adq_list[0]);
+    // }
+
+    // std::string data_directory = "/home/marcus/.local/share/adq-rapid";
+    // std::filesystem::path data_directory_path(data_directory);
+    // if (std::filesystem::exists(data_directory_path))
+    // {
+    //     printf("Path %s exists!\n", data_directory_path.c_str());
+    // }
+    // else
+    // {
+    //     printf("Path does not exist, creating.\n");
+    //     std::error_code e;
+    //     if (std::filesystem::create_directory(data_directory_path, e))
+    //     {
+    //         printf("Created %s.\n", data_directory_path.c_str());
+    //     }
+    //     else
+    //     {
+    //         printf("Failed to create %s, error code %d.\n", data_directory_path.c_str(), e.value());
+    //     }
+    // }
+
     while (!glfwWindowShouldClose(window))
     {
         glfwPollEvents();
-        // glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z
-        // * clear_color.w, clear_color.w);
         glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
         glClear(GL_COLOR_BUFFER_BIT);
         int display_w, display_h;
@@ -137,33 +207,165 @@ int main(int, char **)
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
-        ImGui::BeginMainMenuBar();
-        if (ImGui::MenuItem("Run"))
-        {
-            printf("Pressed run\n");
-            acquisition_a.Initialize(10000, 2, sine_a);
-            processing_a.Initialize();
-            acquisition_b.Initialize(10000, 30, sine_b);
-            processing_b.Initialize();
+        const float TEXT_BASE_WIDTH = ImGui::CalcTextSize("A").x;
+        // const float TEXT_BASE_HEIGHT = ImGui::GetTextLineHeightWithSpacing();
 
-            processing_a.Start();
-            processing_b.Start();
-        }
-        if (ImGui::MenuItem("Stop"))
-        {
-            processing_a.Stop();
-            processing_b.Stop();
-        }
+        ImGui::BeginMainMenuBar();
         if (ImGui::MenuItem("Quit"))
         {
-            printf("Closing\n");
             glfwSetWindowShouldClose(window, 1);
+        }
+        if (ImGui::BeginMenu("Demo"))
+        {
+            ImGui::MenuItem("ImGui", NULL, &show_imgui_demo_window);
+            ImGui::MenuItem("ImPlot", NULL, &show_implot_demo_window);
+            ImGui::EndMenu();
+        }
+        if (ImGui::BeginMenu("Style"))
+        {
+            static int style_idx = 0;
+            if (ImGui::Combo("", &style_idx, "Dark\0Light\0"))
+            {
+                switch (style_idx)
+                {
+                case 0: ImGui::StyleColorsDark(); break;
+                case 1: ImGui::StyleColorsLight(); break;
+                }
+            }
+            ImGui::EndMenu();
         }
         ImGui::EndMainMenuBar();
 
         float frame_height = ImGui::GetFrameHeight();
         ImGui::SetNextWindowPos(ImVec2(0.0, frame_height));
-        ImGui::SetNextWindowSize(ImVec2(display_w / 2, display_h - frame_height));
+        ImGui::SetNextWindowSize(ImVec2(display_w / 2, 200.0));
+        ImGui::Begin("Digitizers", NULL, ImGuiWindowFlags_NoMove);
+        static bool selected[16] = {0};
+
+        if (ImGui::Button("Select All"))
+        {
+            for (int i = 0; i < nof_devices; ++i)
+                selected[i] = true;
+        }
+        ImGui::SameLine();
+        if (ImGui::Button("Deselect All"))
+        {
+            for (int i = 0; i < nof_devices; ++i)
+                selected[i] = false;
+        }
+        ImGui::Separator();
+
+        if (ImGui::BeginTable("Digitizers", 3, ImGuiTableFlags_RowBg | ImGuiTableFlags_Resizable | ImGuiTableFlags_NoSavedSettings))
+        {
+
+            if (nof_devices == 0)
+            {
+                ImGui::TableNextColumn();
+                ImGui::Text("No digitizers found.");
+            }
+            else
+            {
+                ImGui::TableSetupColumn("Identifier", ImGuiTableColumnFlags_WidthFixed | ImGuiTableColumnFlags_NoHide, TEXT_BASE_WIDTH * 20.0f);
+                ImGui::TableSetupColumn("Status", ImGuiTableColumnFlags_WidthFixed, TEXT_BASE_WIDTH * 12.0f);
+                ImGui::TableSetupColumn("Extra");
+                ImGui::TableHeadersRow();
+
+                for (int i = 0; i < nof_devices; ++i)
+                {
+                    char label[32];
+                    std::sprintf(label, "Device %d", i);
+                    ImGui::TableNextColumn();
+                    if (ImGui::Selectable(label, selected[i], ImGuiSelectableFlags_SpanAllColumns))
+                    {
+                        if (!ImGui::GetIO().KeyCtrl)
+                            memset(selected, 0, sizeof(selected));
+                        selected[i] ^= 1;
+                    }
+                    ImGui::TableNextColumn();
+                    ImVec4 color(0.0f, 1.0f, 0.5f, 0.6f);
+                    ImGui::PushStyleColor(ImGuiCol_Button, color);
+                    std::sprintf(label, "OK##%d", i);
+                    if (ImGui::SmallButton(label))
+                    {
+                        printf("OK! %d\n", i);
+                    }
+                    ImGui::PopStyleColor();
+                    ImGui::TableNextRow();
+                }
+            }
+            ImGui::EndTable();
+        }
+        ImGui::End();
+
+        ImGui::SetNextWindowPos(ImVec2(0.0, 200.0 + frame_height));
+        ImGui::SetNextWindowSize(ImVec2(display_w / 2, 200.0));
+        ImGui::Begin("Command Palette");
+        std::stringstream ss;
+        bool any_selected = false;
+        for (int i = 0; i < nof_devices; ++i)
+        {
+            if (selected[i])
+            {
+                if (!any_selected)
+                    ss << "Commands will be applied to device ";
+                else
+                    ss << ", ";
+
+                ss << i;
+                any_selected = true;
+            }
+        }
+
+        if (!any_selected)
+            ss << "No digitizer available.";
+
+        ImGui::Text(ss.str().c_str());
+
+        const ImVec2 COMMAND_PALETTE_BUTTON_SIZE{90, 50};
+        if (ImGui::Button("Start", COMMAND_PALETTE_BUTTON_SIZE))
+        {
+            printf("Start!\n");
+            Start();
+        }
+        ImGui::SameLine();
+        if (ImGui::Button("Stop", COMMAND_PALETTE_BUTTON_SIZE))
+        {
+            printf("Stop!\n");
+            Stop();
+        }
+        ImGui::SameLine();
+        if (ImGui::Button("Set", COMMAND_PALETTE_BUTTON_SIZE))
+        {
+            printf("Set!\n");
+        }
+        ImGui::SameLine();
+        if (ImGui::Button("Get", COMMAND_PALETTE_BUTTON_SIZE))
+        {
+            printf("Get!\n");
+        }
+        ImGui::SameLine();
+        if (ImGui::Button("Initialize", COMMAND_PALETTE_BUTTON_SIZE))
+        {
+            printf("Initialize!\n");
+        }
+        ImGui::SameLine();
+        if (ImGui::Button("Validate", COMMAND_PALETTE_BUTTON_SIZE))
+        {
+            printf("Validate!\n");
+        }
+        if (ImGui::Button("SetPorts", COMMAND_PALETTE_BUTTON_SIZE))
+        {
+            printf("SetPorts!\n");
+        }
+        ImGui::SameLine();
+        if (ImGui::Button("SetSelection", COMMAND_PALETTE_BUTTON_SIZE))
+        {
+            printf("SetSelection!\n");
+        }
+        ImGui::End();
+
+        ImGui::SetNextWindowPos(ImVec2(0.0, 400.0 + frame_height));
+        ImGui::SetNextWindowSize(ImVec2(display_w / 2, display_h - 400.0 - frame_height));
         ImGui::Begin("Parameters", NULL, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize);
         ImGui::InputTextMultiline("##parameters", text, IM_ARRAYSIZE(text),
                                   ImVec2(-FLT_MIN, -FLT_MIN), ImGuiInputTextFlags_AllowTabInput);
@@ -219,11 +421,11 @@ int main(int, char **)
         }
         ImGui::End();
 
-        if (show_demo_window)
-            ImGui::ShowDemoWindow(&show_demo_window);
+        if (show_imgui_demo_window)
+            ImGui::ShowDemoWindow(&show_imgui_demo_window);
 
-        if (show_plot_demo_window)
-            ImPlot::ShowDemoWindow(&show_plot_demo_window);
+        if (show_implot_demo_window)
+            ImPlot::ShowDemoWindow(&show_implot_demo_window);
 
         // Rendering
         ImGui::Render();
@@ -235,6 +437,11 @@ int main(int, char **)
     printf("Stopping\n");
     processing_a.Stop();
     processing_b.Stop();
+
+    if (adq_cu != NULL)
+    {
+        DeleteADQControlUnit(adq_cu);
+    }
 
     // Cleanup
     ImGui_ImplOpenGL3_Shutdown();

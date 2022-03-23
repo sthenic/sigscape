c3371d016655291bd3003f17f576ad097a291b68
Marcus Eriksson <marcus.jr.eriksson@gmail.com>
2022-03-23 22:23:53 +0100

    Add tests for the message queue

---
+++ test/tmessage_thread.cpp
@@ -0,0 +1,225 @@
+#include <thread>
+#include <chrono>
+#include <set>
+#include "message_thread.h"
+
+#include "CppUTest/TestHarness.h"
+
+enum MessageId
+{
+    MESSAGE_ID_IS_STARTED_OK,
+    MESSAGE_ID_FAILED_TO_START,
+    MESSAGE_ID_NEW_DATA,
+    MESSAGE_ID_RETURN_DATA
+};
+
+struct Message
+{
+    enum MessageId id;
+    int code;
+    int *data;
+};
+
+class TestMessageThread : public MessageThread<TestMessageThread, struct Message>
+{
+public:
+    TestMessageThread()
+        : m_start_code(0)
+        , m_nof_messages_to_generate(10)
+    {}
+
+    ~TestMessageThread()
+    {
+        FreeGeneratedData();
+    }
+
+    void Initialize(int start_code, int nof_messages_to_generate)
+    {
+        m_start_code = start_code;
+        m_nof_messages_to_generate = nof_messages_to_generate;
+    }
+
+    void MainLoop()
+    {
+        struct Message start_msg;
+        start_msg.id = (m_start_code != 0) ? MESSAGE_ID_FAILED_TO_START : MESSAGE_ID_IS_STARTED_OK;
+        start_msg.code = m_start_code;
+        start_msg.data = NULL;
+
+        int result = m_read_queue.Write(start_msg);
+        if (result != 0)
+        {
+            printf("Failed to write the start message, result %d.\n", result);
+            m_thread_exit_code = result;
+            return;
+        }
+
+        m_generated_data.clear();
+        m_returned_data.clear();
+
+        for (;;)
+        {
+            /* Continue on 'ok' and 'timeout'. */
+            struct Message read_msg;
+            int result = m_write_queue.Read(read_msg, 10);
+            if ((result != 0) && (result != -1))
+            {
+                m_thread_exit_code = result;
+                break;
+            }
+
+            if (result == 0)
+            {
+                result = HandleMessage(read_msg);
+                if (result != 0)
+                {
+                    printf("Failed to handle message w/ id %d, result %d.\n", read_msg.id, result);
+                    m_thread_exit_code = result;
+                    return;
+                }
+            }
+
+            if (m_nof_messages_to_generate > 0)
+            {
+                int *data = new int[16];
+                for (int i = 0; i < 16; ++i)
+                    data[i] = i;
+
+                m_generated_data.insert(data);
+
+                struct Message write_msg;
+                write_msg.id = MESSAGE_ID_NEW_DATA;
+                write_msg.code = 0;
+                write_msg.data = data;
+
+                result = m_read_queue.Write(write_msg);
+                if (result != 0)
+                {
+                    printf("Failed to write new data message, result %d.\n", result);
+                    m_thread_exit_code = result;
+                    return;
+                }
+
+                m_nof_messages_to_generate -= 1;
+            }
+
+            if (m_should_stop.wait_for(std::chrono::milliseconds(100)) == std::future_status::ready)
+                break;
+        }
+    }
+
+    int VerifyComplete()
+    {
+        /* Check that we got the expected number of replies and that the content
+           matched. */
+
+        auto generated_data = m_generated_data;
+        for (const auto &data : m_generated_data)
+        {
+            if (m_returned_data.count(data) == 0)
+            {
+                printf("%p has not been returned.\n", data);
+                return -1;
+            }
+
+            generated_data.erase(data);
+            m_returned_data.erase(data);
+        }
+
+        if (generated_data.size() > 0)
+        {
+            printf("%zu elements have not been returned.\n", generated_data.size());
+            return -1;
+        }
+
+        if (m_returned_data.size() > 0)
+        {
+            printf("%zu returned elements remain.\n", m_returned_data.size());
+            return -1;
+        }
+
+        FreeGeneratedData();
+        return 0;
+    }
+
+private:
+    std::set<int *> m_generated_data;
+    std::set<int *> m_returned_data;
+    int m_start_code;
+    int m_nof_messages_to_generate;
+
+    int HandleMessage(const struct Message &msg)
+    {
+        /* We only expect to receive messages of this type. */
+        if (msg.id != MESSAGE_ID_RETURN_DATA)
+            return -1;
+
+        m_returned_data.insert(msg.data);
+        return 0;
+    }
+
+    void FreeGeneratedData()
+    {
+        for (auto &v : m_generated_data)
+            delete[] v;
+        m_generated_data.clear();
+    }
+};
+
+
+TEST_GROUP(MessageThread)
+{
+    TestMessageThread thread;
+
+    void setup()
+    {
+    }
+
+    void teardown()
+    {
+        thread.Stop();
+    }
+};
+
+TEST(MessageThread, FailedStart)
+{
+    constexpr int CODE = -88;
+    thread.Initialize(CODE, 10);
+    LONGS_EQUAL(0, thread.Start());
+
+    struct Message msg;
+    LONGS_EQUAL(0, thread.WaitForMessage(msg, 1000));
+    LONGS_EQUAL(MESSAGE_ID_FAILED_TO_START, msg.id);
+    LONGS_EQUAL(CODE, msg.code);
+    CHECK(msg.data == NULL);
+    thread.Stop();
+}
+
+TEST(MessageThread, RevolvingMessages)
+{
+    constexpr int NOF_MESSAGES = 10;
+    thread.Initialize(0, NOF_MESSAGES);
+    LONGS_EQUAL(0, thread.Start());
+
+    /* Expect the 'is started ok' message. */
+    struct Message msg;
+    LONGS_EQUAL(0, thread.WaitForMessage(msg, 500));
+    LONGS_EQUAL(MESSAGE_ID_IS_STARTED_OK, msg.id);
+    LONGS_EQUAL(0, msg.code);
+
+    for (int i = 0; i < NOF_MESSAGES; ++i)
+    {
+        LONGS_EQUAL(0, thread.WaitForMessage(msg, 500));
+        LONGS_EQUAL(MESSAGE_ID_NEW_DATA, msg.id);
+        LONGS_EQUAL(0, msg.code);
+
+        msg.id = MESSAGE_ID_RETURN_DATA;
+        LONGS_EQUAL(0, thread.PostMessage(msg));
+    }
+
+    /* Expect a timeout. */
+    LONGS_EQUAL(-1, thread.WaitForMessage(msg, 500));
+    thread.Stop();
+
+    LONGS_EQUAL(0, thread.VerifyComplete());
+}

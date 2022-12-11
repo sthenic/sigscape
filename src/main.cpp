420103730ffb9e0933801e0cfab56bdc3d91fa8c
Marcus Eriksson <marcus.jr.eriksson@gmail.com>
2022-12-11 22:15:13 +0100

    C++-ify the code interfacing libpng

--- src/main.cpp
+++ src/main.cpp
@@ -16,52 +16,63 @@ static void GlfwErrorCallback(int error, const char *description)
     fprintf(stderr, "Glfw Error %d: %s\n", error, description);
 }
 
-static bool SavePng(const std::string &filename, uint8_t *pixels, int w, int h)
+static bool SavePng(const std::string &filename, uint8_t *pixels, int width, int height)
 {
     /* FIXME: C++-ify */
     png_structp png = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-    if (!png)
+    if (png == NULL)
         return false;
 
     png_infop info = png_create_info_struct(png);
-    if (!info)
+    if (info == NULL)
     {
         png_destroy_write_struct(&png, &info);
         return false;
     }
 
     FILE *fp = fopen(filename.c_str(), "wb");
-    if (!fp)
+    if (fp == NULL)
     {
         png_destroy_write_struct(&png, &info);
         return false;
     }
 
     png_init_io(png, fp);
-    png_set_IHDR(png, info, w, h, 8 /* depth */, PNG_COLOR_TYPE_RGB, PNG_INTERLACE_NONE,
-        PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
-    png_colorp palette = (png_colorp)png_malloc(png, PNG_MAX_PALETTE_LENGTH * sizeof(png_color));
-    if (!palette)
+    png_set_IHDR(png, info, width, height, 8, PNG_COLOR_TYPE_RGB, PNG_INTERLACE_NONE,
+                 PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
+
+    auto palette =
+        static_cast<png_colorp>(png_malloc(png, PNG_MAX_PALETTE_LENGTH * sizeof(png_color)));
+    if (palette == NULL)
     {
         fclose(fp);
         png_destroy_write_struct(&png, &info);
         return false;
     }
+
     png_set_PLTE(png, info, palette, PNG_MAX_PALETTE_LENGTH);
     png_write_info(png, info);
     png_set_packing(png);
 
-    png_bytepp rows = (png_bytepp)png_malloc(png, h * sizeof(png_bytep));
-    for (int i = 0; i < h; ++i)
-        rows[i] = (png_bytep)(pixels + (h - i - 1) * w * 3);
+    auto rows = static_cast<png_bytepp>(png_malloc(png, height * sizeof(png_bytep)));
+    if (rows == NULL)
+    {
+        fclose(fp);
+        png_free(png, palette);
+        png_destroy_write_struct(&png, &info);
+        return false;
+    }
+
+    for (int i = 0; i < height; ++i)
+        rows[i] = static_cast<png_bytep>(pixels + (height - i - 1) * width * 3);
 
     png_write_image(png, rows);
     png_write_end(png, info);
+
+    png_free(png, rows);
     png_free(png, palette);
     png_destroy_write_struct(&png, &info);
-
     fclose(fp);
-    delete[] rows;
     return true;
 }
 

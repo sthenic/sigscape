c4d2edb46373584def871fb91c463a43966c6090
Marcus Eriksson <marcus.jr.eriksson@gmail.com>
2023-03-21 09:10:29 +0100

    Add interface to get a persistent directory to store data

--- include/persistent_directories.h
+++ include/persistent_directories.h
@@ -13,11 +13,15 @@ public:
     /* Get the directory used to store persistent configuration files. */
     const std::string &GetConfigurationDirectory();
 
+    /* Get the directory used to store persistent data files. */
+    const std::string &GetDataDirectory();
+
     /* Get the ImGui initialization file. */
     const char *GetImGuiInitializationFile();
 
 private:
     std::string m_configuration_directory;
+    std::string m_data_directory;
 
     /* Static storage for the ImGui configuration file to be able to have 'const char *'
        as the return type of GetImGuiInitializationFile(). */
--- src/persistent_directories.cpp
+++ src/persistent_directories.cpp
@@ -40,29 +40,51 @@ PersistentDirectories::PersistentDirectories()
     /* FIXME: Linux rules apply? */
     #error "NOT IMPLEMENTED"
 #elif defined(_WIN32)
-    m_configuration_directory = GetEnvOrDefault("APPDATA") + SUFFIX;
+    m_configuration_directory = GetEnvOrDefault("APPDATA") + SUFFIX + "/config";
     ValidateDirectoryCreateIfNeeded(m_configuration_directory);
+
+    m_data_directory = GetEnvOrDefault("APPDATA") + SUFFIX + "/share";
+    ValidateDirectoryCreateIfNeeded(m_data_directory);
 #else
     /* If XDG_CONFIG_HOME is set, we use that, otherwise we default to
        $HOME/.config/sigscape. If we fail to validate the directory, this _has_
        to be because of XDG_CONFIG_HOME not pointing to a writable location.
-       Again, we fall back to HOME/.config. */
-    const auto default_prefix = GetEnvOrDefault("HOME") + "/.config";
-    m_configuration_directory = GetEnvOrDefault("XDG_CONFIG_HOME", default_prefix) + SUFFIX;
+       Again, we fall back to $HOME/.config. */
+    const auto default_configuration_prefix = GetEnvOrDefault("HOME") + "/.config";
+    m_configuration_directory = GetEnvOrDefault("XDG_CONFIG_HOME", default_configuration_prefix) + SUFFIX;
 
     if (!ValidateDirectoryCreateIfNeeded(m_configuration_directory))
     {
-        m_configuration_directory = default_prefix + SUFFIX;
+        m_configuration_directory = default_configuration_prefix + SUFFIX;
         ValidateDirectoryCreateIfNeeded(m_configuration_directory);
+    }
+
+    /* If XDG_DATA_HOME is set, we use that, otherwise we default to
+       $HOME/.local/share/sigscape. If we fail to validate the directory, this
+       _has_ to be because of XDG_DATA_HOME not pointing to a writable location.
+       Again, we fall back to $HOME/.local/share. */
+    const auto default_data_prefix = GetEnvOrDefault("HOME") + "/.local/share";
+    m_data_directory = GetEnvOrDefault("XDG_DATA_HOME", default_data_prefix) + SUFFIX;
+
+    if (!ValidateDirectoryCreateIfNeeded(m_data_directory))
+    {
+      m_data_directory = default_data_prefix + SUFFIX;
+      ValidateDirectoryCreateIfNeeded(m_data_directory);
     }
 #endif
     printf("Using persistent configuration directory '%s'.\n", m_configuration_directory.c_str());
+    printf("Using persistent data directory '%s'.\n", m_data_directory.c_str());
     m_imgui_configuration_file = m_configuration_directory + "/imgui.ini";
 }
 
 const std::string &PersistentDirectories::GetConfigurationDirectory()
 {
     return m_configuration_directory;
+}
+
+const std::string &PersistentDirectories::GetDataDirectory()
+{
+    return m_data_directory;
 }
 
 const char *PersistentDirectories::GetImGuiInitializationFile()
